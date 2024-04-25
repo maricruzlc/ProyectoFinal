cbc8504a4638feef339533ac094824e45c056393
maricruz <maricruzlc5.mlc@gmail.com>
2024-04-25 13:47:35 -0600

    commit
    commit final

---
+++ src/main/java/com/Proyecto/dao/CategoriaDao.java
@@ -0,0 +1,12 @@
+package com.Proyecto.dao;
+
+import com.Proyecto.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author PC MASTER
+ */
+public interface CategoriaDao extends JpaRepository<Categoria, Long> {
+
+}
---
+++ src/main/java/com/Proyecto/dao/RolDao.java
@@ -0,0 +1,8 @@
+package com.Proyecto.dao;
+
+import com.Proyecto.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
